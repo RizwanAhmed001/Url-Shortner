eab582876ba660bc85baf1c342fdf037f6c306e8
Rizwan Ahmed <rizwanahmedrls96@gmail.com>
2026-04-29 18:36:41 +0530

    login api is added

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -66,8 +66,6 @@ export const register = async (req, res) => {
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    console.log("Token sai pahle!")
-
     const newUser = new UserModel({
       name,
       email,
@@ -82,18 +80,61 @@ export const register = async (req, res) => {
     }
 
     const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-      expiresIn: "2d",
+      expiresIn: "5d",
     });
-
-    console.log("Token sai bad");
 
     return res.status(201).json({
       success: true,
-      message: "User Registered Successfully!",
+      message: "Registered Successfully!",
       user: { name: newUser.name, image: newUser.profile },
       token,
     });
   } catch (error) {
-    return res.status(500).json({ success: false, message: error.message + "gfvdhg"});
+    return res
+      .status(500)
+      .json({ success: false, message: error.message + "gfvdhg" });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, message: "All Fields Are Mandatory!" });
+    }
+
+    const user = await UserModel.findOne({ email });
+
+    if (!user) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Invalid Credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Invalid Credentials!" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "5d",
+    });
+
+    return res
+      .status(200)
+      .json({
+        success: true,
+        message: "Login Successfully!",
+        user: { name: user.name, image: user.profile },
+        token,
+      });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
   }
 };
