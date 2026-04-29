b5138fe57b453ae173354cb97c9ce99608dc2c8d
Rizwan Ahmed <rizwanahmedrls96@gmail.com>
2026-04-29 18:42:09 +0530

    logout api is added

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import uploadImage from "../middleware/multer.js";
-import { login, register } from "../controllers/user.controller.js";
+import { login, logout, register } from "../controllers/user.controller.js";
 
 const userRoute = express.Router();
 
 userRoute.post("/register", uploadImage.single("image"), register);
 userRoute.post("/login", login);
+userRoute.post("/logout", logout);
 
 export default userRoute;
