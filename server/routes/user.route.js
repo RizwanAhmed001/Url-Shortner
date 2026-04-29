e409cf5a5dde1de633716054ad46fb75c6bb649d
Rizwan Ahmed <rizwanahmedrls96@gmail.com>
2026-04-29 18:39:02 +0530

    login api is tested working fine

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import uploadImage from "../middleware/multer.js";
-import { register } from "../controllers/user.controller.js";
+import { login, register } from "../controllers/user.controller.js";
 
 const userRoute = express.Router();
 
 userRoute.post("/register", uploadImage.single("image"), register);
+userRoute.post("/login", login);
 
 export default userRoute;
