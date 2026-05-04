eaeeee82baf066ae59fc3654024b4fee9cbe18d2
Rizwan Ahmed <rizwanahmedrls96@gmail.com>
2026-05-04 20:10:07 +0530

    auth middleware is added

--- server/routes/url.route.js
+++ server/routes/url.route.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { addUrl } from "../controllers/url.controller.js";
+import { auth } from "../middleware/auth.js";
 
 const urlRoute = express.Router();
 
-urlRoute.post("/addurl", addUrl);
+urlRoute.post("/addurl",auth, addUrl);
 
 export default urlRoute;
