b3426dcef25ea275226758ac5ffdc55139915a1e
Rizwan Ahmed <rizwanahmedrls96@gmail.com>
2026-05-12 18:45:08 +0530

    Add urlClicks route to url.route.js

--- server/routes/url.route.js
+++ server/routes/url.route.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { addUrl, allUserUrls, redirectUrl } from "../controllers/url.controller.js";
+import { addUrl, allUserUrls, urlClicks } from "../controllers/url.controller.js";
 import { auth } from "../middleware/auth.js";
 
 const urlRoute = express.Router();
 
 urlRoute.post("/addurl",auth, addUrl);
 urlRoute.get("/alluserurls", auth, allUserUrls);
-
+url.Route.get("/urlclick", auth, urlClicks);
 
 export default urlRoute;
